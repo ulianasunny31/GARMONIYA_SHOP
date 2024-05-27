525f93a97ee9b2cf03cb44053b06a64a5c3dd668
Uliana Seredenko <uliana1331@gmail.com>
2024-05-27 10:09:52 +0300

    rewards start +js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 // Показ вторых картинок при ховере
-document.querySelectorAll(".balls-list li").forEach((item) => {
-  const imgOne = item.querySelector(".image-one");
-  const imgTwo = item.querySelector(".image-two");
+document
+  .querySelectorAll(".balls-list li")
+  .forEach((item) => showAndHide(item));
 
-  function mouseIn() {
-    imgTwo.classList.replace("not-visible", "visible");
-    imgOne.classList.replace("visible", "not-visible");
-  }
-
-  function mouseOut() {
-    imgTwo.classList.replace("visible", "not-visible");
-    imgOne.classList.replace("not-visible", "visible");
-  }
-
-  item.addEventListener("mouseover", mouseIn);
-  item.addEventListener("mouseout", mouseOut);
-});
-
+document
+  .querySelectorAll(".rewards-list li")
+  .forEach((item) => showAndHide(item));
 //Категории товаров
 const categories = document.querySelectorAll(".categories-list li");
 const refs = {
@@ -24,6 +13,7 @@ const refs = {
   balls: document.querySelector("balls-container"),
 };
 
+//??? НЕПРАВИЛЬНО НУЖЕН ОБРАБОТЧИК НАЖАТИЯ
 categories.forEach((item) => {
   const category = item.textContent;
   console.log(category);
@@ -42,3 +32,21 @@ categories.forEach((item) => {
       break;
   }
 });
+
+function showAndHide(i) {
+  const imgOne = i.querySelector(".image-one");
+  const imgTwo = i.querySelector(".image-two");
+
+  function mouseIn() {
+    imgTwo.classList.replace("not-visible", "visible");
+    imgOne.classList.replace("visible", "not-visible");
+  }
+
+  function mouseOut() {
+    imgTwo.classList.replace("visible", "not-visible");
+    imgOne.classList.replace("not-visible", "visible");
+  }
+
+  i.addEventListener("mouseover", mouseIn);
+  i.addEventListener("mouseout", mouseOut);
+}
