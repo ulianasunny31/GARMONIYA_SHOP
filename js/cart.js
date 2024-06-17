c6d6087be931036ab7ef45b30d92b6c56ebcd1f3
Uliana Seredenko <uliana1331@gmail.com>
2024-06-17 18:33:50 +0300

    cart work

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-const cartSection = document.querySelector(".main-cart-section");
+const myCart = document.querySelector(".my-cart-list");
 
 const cart = JSON.parse(localStorage.getItem("cart")) || [];
 console.log(cart);
@@ -8,3 +8,20 @@ if (cart.length === 0) {
     <h1 class="empty-heading">Ваш кошик пустий.</h1>
     `;
 }
+
+cart.forEach((product) => {
+  const productHTML = `
+  <li class="cart-product">
+  <img src="${product.image}" alt="${product.name}" class="cart-product-image">
+  <h2 class="cart-product-name">${product.name}</h2>
+  <p class="cart-product-price">${product.price} грн</p>
+  <div class='cart-item-btns'>
+  <button class="cart-product-remove" data-id="${product.id}">+</button>
+  <p class="cart-product-quantity">Кількість: ${product.quantity}</p>
+  <button class="cart-product-remove" data-id="${product.id}">-</button>
+  </div>
+  </li>
+  `;
+
+  myCart.innerHTML = productHTML;
+});
--- js/script.js
+++ js/script.js
@@ -108,18 +108,32 @@ addBTns.forEach((btn) => {
     const productName = product.querySelector(".goods-name-p").textContent;
     const productPrice = product.querySelector(".price-div p").textContent;
     const picture = product.querySelector(".image-one").src;
+    const productId = generateUniqueId(productName, productPrice);
 
     const fullProduct = {
       name: productName,
       price: productPrice,
+      id: productId,
       picture,
+      quantity: 1,
     };
 
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push(fullProduct);
+
+    const existingProduct = cart.findIndex((item) => item.id === productId);
+
+    if (existingProduct !== -1) {
+      cart[existingProduct].quantity += 1;
+    } else {
+      cart.push(fullProduct);
+    }
 
     localStorage.setItem("cart", JSON.stringify(cart));
-    // console.log("Товар добавлен в корзину:", fullProduct);
-    // console.log("Текущая корзина:", cart);
+    console.log("Товар добавлен в корзину:", fullProduct);
+    console.log("Текущая корзина:", cart);
   });
 });
+
+function generateUniqueId(name, price) {
+  return name + price;
+}
